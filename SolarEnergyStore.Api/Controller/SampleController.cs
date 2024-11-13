b7c57a01cb219de86519b6874eb21b4eb17da276
GBFerro <giovaniferro29@gmail.com>
2024-11-12 22:58:49 -0300

    feat: criado estrutura inicial do projeto para criacao de pipeline

---
+++ SolarEnergyStore.Api/Controller/SampleController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarEnergyStore.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+    }
+}
